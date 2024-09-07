76a8eced831c14e1e53bfb3a560580986bbdf025
takanosatomi <ricebread778899@gmail.com>
2024-09-07 12:00:23 +0900

    コメント追加

--- javascripts/ajax_sample.js
+++ javascripts/ajax_sample.js
@@ -20,7 +20,7 @@ function changeVideo() {
     //初回クリックはdataが空
     if (data.length === 0) {
       getData(); // データを取得して更新
-    } else {
+    } else {//初回ではない場合
       updateData();// 取得済みのデータを使用して画面を更新
     }
   });
@@ -32,21 +32,21 @@ window.onload = changeVideo;
 // 非同期通信を管理するXMLHttpRequestオブジェクトを使ってファイルから内容を取得
 function getData() {
   // 1.XMLHttpRequestオブジェクトを生成し、サーバーとの非同期通信を行う準備をする
-  const request = new XMLHttpRequest(); //-- 1
+  const request = new XMLHttpRequest(); //XMLHttpRequestオブジェクトが入ったrequestオブジェクトを使い、非同期通信が行われる
 
-  //2.onreadystatechangeプロパティで非同期通信開始から終了までに実行すべき処理を定義。
+  //2.onreadystatechangeプロパティで非同期通信開始から終了までに実行すべき処理を定義。XMLHttpRequestのプロパティ
   //  onreadystatechange:サーバとの通信状態に変化があった(レスポンスが返ってきた)時に呼び出され、登録した処理を実行するイベントハンドラ
   //①requestオブジェクトにどういう処理をするか決める
   request.onreadystatechange = function () {
     //-- 2-1
-    //非同期通信が完了した場合
-    if (request.readyState == 4) {
-      // readyStateプロパティの戻り値が4になった時、つまり非同期通信が完了した時にレスポンスデータを使い画像を更新
+    //非同期通信が完了していたら
+    if (request.readyState == 4) {//readyStateプロパティの戻り値が4になった時、つまり非同期通信が完了した時に
+      // レスポンスデータを使い画像を更新
       //サーバからの応答が正常だった場合
-      if (request.status == 200) {
-        //statusプロパティの戻り値は、HTTPのステータスコードをあわらす。ステータスコード200:リクエストが成功
+      if (request.status == 200) {//ステータスコード200:リクエストが成功
+        //statusプロパティの戻り値は、HTTPのステータスコードをあわらす。
         // レスポンスデータを変数に格納
-        data = request.response;
+        data = request.response;//response: 応答データを持つ、XMLHttpRequestのプロパティ
         updateData(); // データ取得後にページを更新
       }
     }
@@ -55,15 +55,19 @@ function getData() {
      request.open("GET", "ajax.json"); //open()メソッドが呼ばれると、リクエストの準備が完了
      request.responseType = "json"; //サーバーからのレスポンスデータをJSON形式として受け取るように設定
  
-     //②その処理を実行する
+     //②サーバに送信する
      request.send(null); //openメソッドで初期化されたHTTPリクエストをサーバーに送信する
   
 }
 
+
+
 // サーバからのレスポンスを画像データに反映させる
-function updateData() {
-  titleArea.innerHTML = data[number].title; //
+function updateData() {//
+  titleArea.innerHTML = data[number].title; //innerHTMLプロパティは要素内の全てをタグやテキスト付きのHTMLとして取得する。
+  // ここではタグ要素間のテキストを右辺のテキストに置き換える
+  // <h1 id="title">ここにdata[number].titleの文字列を入れる</h1>
   contentArea.innerHTML = data[number].content; //
   videoArea.setAttribute("src", data[number].url); //
-  number == 2 ? (number = 0) : number++; //--7
+  number == 2 ? (number = 0) : number++; //最初のデータ(number = 0)がここで次のデータ(number = 1)になる。最後のデータが表示されたら、次は最初のデータに戻す
 }
